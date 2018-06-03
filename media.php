55a0d1e70b5527db2364bdf5dd0978eb60d3982d
Quinn Casey <qcasey1@gmail.com>
2018-06-03 14:34:30 -0700

    output

--- media.php
+++ media.php
@@ -1,6 +1,8 @@
 <?php
 // Controller for BT media
 
-print_r(json_encode(exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track")));
+$output = array();
+exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
+print_r(json_encode($output));
 
 ?>
