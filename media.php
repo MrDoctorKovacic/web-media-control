9e9cd35f0fe59900bce63eb9e3c313fb7a2d38a8
Quinn Casey <qcasey1@gmail.com>
2018-06-03 15:38:24 -0700

    proper regex?

--- media.php
+++ media.php
@@ -20,7 +20,7 @@ foreach($output as $key => $item) {
             // Get the value if we've matched a key
             preg_match('/\"(.*?)\"/', $output[$key+1], $m2);
             if(empty($m2)) {
-                preg_match('/(variant uint32) (.*?)/', $output[$key+1], $m2);
+                preg_match('/^(variant uint32) (.*?)$/', $output[$key+1], $m2);
                 print_r($m2);
             }
 
