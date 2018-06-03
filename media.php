9ffe14d0eea006a10d29cab7d1a63e5d5ce93046
Quinn Casey <qcasey1@gmail.com>
2018-06-03 15:31:29 -0700

    temp oops

--- media.php
+++ media.php
@@ -19,9 +19,8 @@ foreach($output as $key => $item) {
 
             // Get the value if we've matched a key
             preg_match('/\"(.*?)\"/', $output[$key+1], $m2);
-            echo($key);
             if(empty($m2)) {
-                //preg_match('/variant uint32 (.*?)/', $output[$key+1], $m2);
+                preg_match('/variant uint32 (.*?)/', $output[$key+1], $m2);
             }
 
             $real_output[$m[1]] = $m2;
