67b0c6255442a8fc666a106e21a850d34db88d0b
Quinn Casey <qcasey1@gmail.com>
2018-06-03 15:51:24 -0700

    switch it up

--- media.php
+++ media.php
@@ -3,38 +3,46 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$output = array();
-exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
-print_r($output);
-$real_output = array();
+function getMediaInfo() {
+    $output = array();
+    exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
+    $real_output = array();
 
-$onKey = True;
-foreach($output as $key => $item) {
+    $onKey = True;
+    foreach($output as $key => $item) {
 
-    // Only get keys, skip values of this weird array
-    if($onKey) {
-        if (preg_match('/\"(.*?)\"/', $item, $m)) {
+        // Only get keys, skip values of this weird array
+        if($onKey) {
+            if (preg_match('/\"(.*?)\"/', $item, $m)) {
 
-            unset($m2);
+                unset($m2);
 
-            // Get the value if we've matched a key
-            preg_match('/\"(.*?)\"/', $output[$key+1], $m2);
-            if(empty($m2)) {
-                preg_match('/([0-9]*)$/', utf8_encode($output[$key+1]), $m2);
-                //print_r($m2);
-                //print_r($output[$key+1]);
+                // Get the value if we've matched a key
+                preg_match('/\"(.*?)\"/', $output[$key+1], $m2);
+                if(empty($m2)) {
+                    preg_match('/([0-9]*)$/', utf8_encode($output[$key+1]), $m2);
+                    //print_r($m2);
+                    //print_r($output[$key+1]);
+                }
+
+                $real_output[$m[1]] = $m2[1];
+
+                // Skip next value
+                $onKey = False;
             }
-
-            $real_output[$m[1]] = $m2[1];
-
-            // Skip next value
-            $onKey = False;
+        } else {
+            $onKey = True;
         }
-    } else {
-        $onKey = True;
     }
+    return json_encode($real_output);
 }
 
-print_r($real_output);
+switch ($_GET["command"]) {
+    case "info":
+    default:
+        print(getMediaInfo());
+        break;
+}
+
 
 ?>
