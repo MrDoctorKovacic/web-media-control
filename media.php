c947311f014e252f1ac9383d763333ef5025fe11
Quinn Casey <qcasey1@gmail.com>
2018-06-03 14:37:42 -0700

    explode

--- media.php
+++ media.php
@@ -3,6 +3,6 @@
 
 $output = array();
 exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
-print_r(json_encode($output));
+print_r(json_encode(implode($output, " ")));
 
 ?>
