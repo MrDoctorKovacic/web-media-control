86f8ccacc8d3116900f9aa22f071020fcfc1dc25
Quinn Casey <qcasey1@gmail.com>
2018-06-03 14:50:26 -0700

    changes

--- media.php
+++ media.php
@@ -4,5 +4,14 @@
 $output = array();
 exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
 print_r($output);
+$real_output = array()
+for_each($key->$item in $output) {
+    if(strpos($item, "string") !== false ) {
+        $item_parse = explode($item, " ");
+        $real_output[$output[$key+1]] = $real_output[$item[2]]
+    }
+}
+
+print_r($real_output);
 
 ?>
