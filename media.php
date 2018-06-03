2b6ed840d2fd6612dcec8012cc6675d338e99334
Quinn Casey <qcasey1@gmail.com>
2018-06-03 15:00:39 -0700

    new regex

--- media.php
+++ media.php
@@ -1,5 +1,7 @@
 <?php
 // Controller for BT media
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $output = array();
 exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track", $output);
@@ -7,7 +9,7 @@ print_r($output);
 $real_output = array();
 
 foreach($output as $key => $item) {
-    if (preg_match('/("")/', $item, $m)) {
+    if (preg_match('~(["\'])([^"\']+)\1~', $item, $m)) {
         //print $m[1];
         //$item_parse = explode($item, " ");
         array_push($m[1]);
