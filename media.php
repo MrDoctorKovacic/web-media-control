d156abd82f940460a4649595be208f56ca402e4b
Quinn Casey <qcasey1@gmail.com>
2018-06-03 14:32:31 -0700

    jsonified

--- media.php
+++ media.php
@@ -1,6 +1,6 @@
 <?php
 // Controller for BT media
 
-print_r(exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track"));
+print_r(json_encode(exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track")));
 
 ?>
