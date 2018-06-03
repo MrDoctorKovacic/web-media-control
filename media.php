ba805867275dc124d65294c17e231177bcbb0f9d
Quinn Casey <qcasey1@gmail.com>
2018-06-03 14:30:19 -0700

    testing dbus 2

--- media.php
+++ media.php
@@ -1,6 +1,6 @@
 <?php
 // Controller for BT media
 
-echo exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Status");
+echo exec("dbus-send --system --print-reply --type=method_call --dest=org.bluez /org/bluez/hci0/dev_4C_32_75_AD_98_24/player0 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track");
 
 ?>
